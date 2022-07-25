376129137b5dbb4b80364f4d13f89e7f3c42f156
JoonHyukSuh <joon102938@gmail.com>
2022-07-25 15:53:42 +0900

    Update JoonHyuk.py

--- JoonHyuk.py
+++ JoonHyuk.py
@@ -1,1 +1,8 @@
-test = print('test')
+import telegram
+
+api_key = '5531979807:AAH6Jk92UkyuVpLc6qjBxfR_rVAK1cmBG3c'
+
+bot = telegram.Bot(token = api_key)
+
+chat_id = bot.get_updates()[-1].message.chat_id
+print(chat_id)
