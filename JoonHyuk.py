2bcab2f3385bf9826daf77f6c75557f6a74775bd
JoonHyukSuh <joon102938@gmail.com>
2022-08-04 20:15:33 +0900

    .

--- JoonHyuk.py
+++
@@ -1,8 +0,0 @@
-import telegram
-
-api_key = '5531979807:AAH6Jk92UkyuVpLc6qjBxfR_rVAK1cmBG3c'
-
-bot = telegram.Bot(token = api_key)
-
-chat_id = bot.get_updates()[-1].message.chat_id
-print(chat_id)
