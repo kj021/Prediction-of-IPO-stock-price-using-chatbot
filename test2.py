8d90fd4df00aa45b11bcf8dfb68d33ed169eb981
Hyun Soo Kim <93048764+KIMHYUNSOO1999@users.noreply.github.com>
2022-08-12 12:49:51 +0900

    뉴스(검색,구독),공시(검색) 구현

--- test2.py
+++ test2.py
@@ -9,9 +9,8 @@ import pickle
 import numpy as np
 from database.predict_database import get_data_csv
 from DART.Search_report import get_find_Report
-# from naver_news.news import find_news
+from naver_news.news import find_news_1
 from draw_graph.draw import get_graph
-# from naver_news.subscribe_news import find_news,Stop_find_news
 from apscheduler.schedulers.blocking import BlockingScheduler
 from naver_news.config import client_id,client_secret
 import time
@@ -21,26 +20,22 @@ import requests
 
 
 
-
+# 몽고디비,데이터모델,텔레그램 호환
 bot = telegram.Bot(token = api_key)
 BASE_PATH  = os.getcwd()
 
-info_message = '''다음의 명령어를 입력해주세요.
-
-- 안부 물어보기 : 뭐해
-- 공모주 가격 물어보기 : 공모주 + "기업명"
-- 차트 보기 : "기업명" + 차트
-- 사진 보기 : 사진
-'''
 client = MongoClient('localhost', 27017)
 db = client['Ipo']
-
-
 
 with open('regression/saved_model.pickle','rb') as f:
         model3 = pickle.load(f)
 
-        
+
+#뉴스기사 보관 리스트
+subscribe=[]       
+old_links=[]    
+
+Flag_toggle=0
 
 
 # bot.sendMessage(chat_id = chat_id,text=info_message)
@@ -71,12 +66,12 @@ def get_price(cor_name):
         
     return price_origin,price,y_predict
 
-old_links=[]    
+#뉴스 구독리스트 함수
 
 def find_news():
-    print("111")
+    
     for row in subscribe:
-        print("-진행중-")
+        print("---------진행중-----------")
         result_title=""
         search_word = row #검색어
         encode_type = 'json' #출력 방식 json 또는 xml
@@ -128,30 +123,20 @@ def find_news():
       
         check=0
     time.sleep(1)
-    
-subscribe=[]
-# reset=0
-# flag=0
-# reset=0
-
-# flag_stop=0
 
 
+info_message = '''다음의 명령어를 입력해주세요.
 
-def start(update, context):
-        
+- 안부 물어보기 : 뭐해
+- 공모주 가격 물어보기 : 공모주 + "기업명"
+- 차트 보기 : "기업명" + 차트
+- 사진 보기 : 사진
+'''
+
+def start(update, context):       
     bot.sendMessage(chat_id =update.effective_chat.id,text='안녕하세요 IPO 공모가 예측 봇 Stock-Manager 입니다.') # 채팅방에 입장했을 때, 인사말 
     bot.sendMessage(chat_id=update.effective_chat.id, text=info_message)
 
-
-# def stop(update, context):
-#     bot.sendMessage(chat_id =update.effective_chat.id,text='<명령어 실행>') # 채팅방에 입장했을 때, 인사말 
-   
-#     if flag==0: 
-#         t=threading.Timer(30,find_news(subscribe))
-#         t.start() 
-#     if flag==1: 
-#         t.cancel() 
         
 def handler(update, context):
     user_text = update.message.text # 사용자가 보낸 메세지를 user_text 변수에 저장합니다.
@@ -168,37 +153,7 @@ def handler(update, context):
     elif '차트종류' in user_text:
        bot.send_message(chat_id=update.effective_chat.id, text=f"1.경쟁률\n2.의무보유확약\n3.청약경쟁률\n4.확정공모가\n")
        
-    elif '공시' in user_text: 
-        # except 발생안함 -> 내부 안에서 변경해야함
-        cor_name = user_text.split()[1]
-        count = int(user_text.split()[2])
-        Text=[]
-        Text.clear() 
-        
-        try:       
-            Text=get_find_Report(cor_name,count)
-            result_Text = "\n".join(Text)
-            bot.send_message(chat_id=update.effective_chat.id, text=f"공시정보\n\n{result_Text}")
-        except KeyError:
-            bot.send_message(chat_id=update.effective_chat.id, text="수집되지 않은 정보입니다.")
-        
-    elif '뉴스' in user_text:
-        # except 발생안함 -> 내부 안에서 변경해야함
-        cor_name = user_text.split()[1]
-        count = int(user_text.split()[2])
-        
-     
-        Text2=[]
-        Text2.clear()        
-        Text2=find_news(cor_name,count)
-        
-        try:
-            for rows in Text2:
-                bot.send_message(chat_id=update.effective_chat.id, text=f"{rows}")
-        except KeyError:
-            bot.send_message(chat_id=update.effective_chat.id, text="수집되지 않은 정보입니다.")
-                         
-                  
+    
     elif '차트' in user_text:
         
         cor_name = user_text.split()[1]
@@ -215,86 +170,109 @@ def handler(update, context):
         except KeyError:
             bot.send_message(chat_id=update.effective_chat.id, text="수집되지 않은 정보입니다.")
         
-    
     elif '예측' in user_text:
         
         cor_name = user_text.split()[1]
         price,result_price,result_per=get_price(cor_name)
         bot.send_message(chat_id=update.effective_chat.id, text=f"<{cor_name}>\n공모가:  {price}\n예측 시초가:  {result_price}\n예상 수익률:  {result_per}%")
     
+    
+    #다트(검색)
+    elif '/Search_Dart' in user_text: 
+        # except 발생안함 -> 내부 안에서 변경해야함
+        cor_name = user_text.split()[1]
+        cor_name_len = len(user_text.split())
+        
+        if cor_name_len<=2 or user_text.split()[2].isnumeric() == False:
+            count=3
+        else:
+            count = int(user_text.split()[2])
+            
+        Text=[]
+        Text.clear() 
+        
+        try:       
+            Text=get_find_Report(cor_name,count)
+            result_Text = "\n".join(Text)
+            bot.send_message(chat_id=update.effective_chat.id, text=f"공시정보\n\n{result_Text}")
+        except KeyError:
+            bot.send_message(chat_id=update.effective_chat.id, text="수집되지 않은 정보입니다.")
+                       
+    
+    
+    #뉴스(검색) -> 완료
+    elif '/Search_News' in user_text:
+        # except 발생안함 -> 내부 안에서 변경해야함
+        cor_name = user_text.split()[1]
+        cor_name_len = len(user_text.split())
+        
+        # 검색량(기사) 디폴트 : 3
+        if cor_name_len<=2 or user_text.split()[2].isnumeric() == False:
+            count=3
+        else:
+            count = int(user_text.split()[2])
+        
+     
+        Text2=[]
+        Text2.clear()        
+        Text2=find_news_1(cor_name,count)
+        
+        for rows in Text2:
+            bot.send_message(chat_id=update.effective_chat.id, text=f"{rows}")
+        
+       
+    # 뉴스(구독) -> 완료
     elif '/sub' in user_text:
         cor_name = user_text.split()[1]
         subscribe.append(cor_name)
-        print(subscribe)
         
         bot.send_message(chat_id=update.effective_chat.id, text=f"<뉴스구독리스트>\n <{cor_name}> 추가되었습니다.")
     
-    elif '/list' in user_text:
-        bot.send_message(chat_id=update.effective_chat.id, text=f"<뉴스구독리스트>\n {subscribe}")
+    elif user_text == '/list' :
+        Text2 = "\n".join(subscribe)
+        bot.send_message(chat_id=update.effective_chat.id, text=f"<뉴스구독리스트>\n\n{Text2}")
     
     
-    elif '/del' in user_text:
+    elif '/unsub' in user_text:
         
         cor_name = user_text.split()[1]
         subscribe.remove(cor_name)
         print(subscribe)
         
         bot.send_message(chat_id=update.effective_chat.id, text=f"<뉴스구독리스트>\n <{cor_name}> 삭제됐습니다.")     
-   
-    elif user_text=='종료':
-        print("CHECK")
+     
+           
+#뉴스(구독) 스위치 -> 완료      
+def News_toggle(update, context):       
+    global Flag_toggle
+        
+    if Flag_toggle==0:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="작업을 시작합니다.")
+ 
+        print("뉴스(구독) 시작")
+        sched.add_job(find_news, 'interval', seconds = 10, id='my_job_id')
+        Flag_toggle=1
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="작업을 중단합니다.")
+ 
+        print("뉴스(구독) 종료")
         sched.remove_job('my_job_id')
-        
-        
-       
-        
-    elif user_text=='시작':
-        sched.add_job(find_news, 'interval', seconds = 3, id='my_job_id')
-        
-            
-    # elif user_text=='수정':
-    #     global reset
-    #     reset=1
-    #     print(flag_stop)      
-
-
+        Flag_toggle=0
+             
    
 start_handler = CommandHandler('start',start)
+toggle_handler = CommandHandler('News_toggle',News_toggle) 
 echo_handler = MessageHandler(Filters.text,handler) # chatbot에게 메세지를 전송하면,updater를 통해 필터링된 text가 handler로 전달이 된다. -> 가장 중요하고, 계속해서 수정할 부분
-# stop_handler = CommandHandler('stop',stop) 
 
-updater.dispatcher.add_handler(start_handler)
-updater.dispatcher.add_handler(echo_handler)
-# updater.dispatcher.add_handler(stop_handler)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(toggle_handler)
+dispatcher.add_handler(echo_handler)
+
 
 updater.start_polling() # 주기적으로 텔레그램 서버에 접속해서 chatbot으로부터 새로운 메세지가 존재하면 받아오는 명령어.
 
-# flag_t=0
-# print("--------실행중-----\n")
-
-# while True:
-#     print(subscribe)
-#     if subscribe!=None and flag_t==0 and flag_stop==1:
-#         print("시작")
-#         t=threading.Timer(3,find_news(subscribe))
-#         t.start()
-#         flag_t=1
     
-#     if reset==1 and flag_t==1:
-#         print("종료(수정")
-#         t.cancel()
-#         reset=0
-#         flag_t=0
-        
-
-#     if flag_t==1 and flag_stop==0:
-#         print("종료")
-#         t.cancel()
-#         flag_t=0 
-            
-        
-    # print(flag_t,flag_stop,reset)
-    # time.sleep(2)
-
 sched = BlockingScheduler(timezone='Asia/Seoul')
 sched.start()
+ 
