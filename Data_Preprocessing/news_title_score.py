b4b62e647cd97fea04fa6592fe735926c4a96871
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-08-14 16:13:38 +0900

    0814

--- Data_Preprocessing/news_title_score.py
+++ Data_Preprocessing/news_title_score.py
@@ -2,21 +2,38 @@ import pandas as pd
 
 def get_news_score():
     
+
+
+
+    
     data = pd.read_csv('/Users/seop/Documents/GitHub/Prediction-of-IPO-stock-price-using-chatbot/jiseop_test/after_prepros_get_label.csv')
+
     print(data.columns)
     data.drop(['Unnamed: 0'], axis = 1,inplace = True)
 
+
+
     data['label']=data['label'].replace('negative','-1').replace('neutral','0').replace('positive','1')
     data = data.astype({'label':'int64'})
-
+    data
     df_sum=data.groupby("cor_name").sum()
-    df_sum['count']=data.groupby("cor_name")['title'].count()
-    df_sum['score']=round(df_sum['label']/df_sum['count'],3)
 
-    # df_sum = df_sum.rename(columns={'label': '점수''count':'합계','score':'총점'})
-    del df_sum['count']
-    del df_sum['label']
-    df_sum.to_csv('/Users/seop/Documents/GitHub/Prediction-of-IPO-stock-price-using-chatbot/jiseop_test/after_prerpos_get_score.csv',encoding="utf-8-sig")
+    df_count=data.groupby("cor_name").count()
+
+
+    df_sum['score'] = round(df_sum['label']/df_count['label'],2)
+
+
+    temp_df = pd.DataFrame(df_sum['score'])
+
+    temp_df = temp_df .reset_index()
+
+
+
+    df = pd.read_csv('/Users/seop/Documents/GitHub/Prediction-of-IPO-stock-price-using-chatbot/jiseop_test/after_prepros.csv').drop('Unnamed: 0',axis = 1)
+    df = df.merge(temp_df,on='cor_name')
+
+    df.to_csv('/Users/seop/Documents/GitHub/Prediction-of-IPO-stock-price-using-chatbot/jiseop_test/after_prerpos_get_score.csv',encoding="utf-8-sig")
 
 
 get_news_score()
