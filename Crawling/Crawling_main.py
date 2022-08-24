f1a8493d8f2644912da93a22771c063846ef3157
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-08-24 15:07:21 +0900

    0824 크롤링코드 전체 수합

--- Crawling/Crawling_main.py
+++ Crawling/Crawling_main.py
@@ -15,31 +15,39 @@ def test():
     from get_title_score import get_title_score2
     from news_title_score import get_news_score
     from Crawling_per import get_per
+    from Crawling_search_amt import main_search_amt
 
     from pathlib import Path
     BASE_DIR = Path(__file__).resolve().parent.parent
 
-    crawling_38_basic_info(BASE_DIR)
+    # crawling_38_basic_info(BASE_DIR)
 
-    crawling_38_day(BASE_DIR)
+    # crawling_38_day(BASE_DIR)
 
-    crawling_data(BASE_DIR)
+    # crawling_data(BASE_DIR)
 
-    preprocessing()
+    # preprocessing()
 
-    df = pd.DataFrame(
-        columns=[
-            "cor_name",
-            "title",
-        ]
-    )
-    start_time = time.time()
+    # df = pd.DataFrame(
+    #     columns=[
+    #         "cor_name",
+    #         "title",
+    #     ]
+    # )
+    # start_time = time.time()
 
-    asyncio.run(news_main(df))
+    # asyncio.run(news_main(df))
 
-    get_title_score2()
-    get_news_score()
-    get_per()
+    # get_title_score2()
+    # get_news_score()
+    # get_per()
+    
+    total_df = pd.read_csv(BASE_DIR/'Crawling/after_prepros_get_score.csv')
+    result = main_search_amt(total_df)
+    print(result.head())
+
+
+
     
 
 
